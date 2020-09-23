4be61d36044e9e96a909ab26d4ee4f2d13e91948
Elytrion <electronfirework@gmail.com>
2020-09-23 16:45:32 -0700

    Added a teleport reticle and the TeleportRayActivator, which controls the activation of the teleporter rays.

---
+++ unityxrex/Assets/Scripts/TeleportRayActivator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TeleportRayActivator : MonoBehaviour
+{
+    [SerializeField] private XRController _leftTeleportRay;
+    [SerializeField] private XRController _rightTeleportRay;
+    [SerializeField] private InputHelpers.Button _teleportActivationBtn;
+    [SerializeField] private float _activationThreshold = 0.1f;
+
+    void Update()
+    {
+        CheckForRayActivation(_leftTeleportRay);
+        CheckForRayActivation(_rightTeleportRay);
+    }
+
+    private bool CheckIfTeleportActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, _teleportActivationBtn, out bool isActive, _activationThreshold);
+        return isActive;
+    }
+
+    private void CheckForRayActivation(XRController controller)
+    {
+        if(controller)
+        {
+            controller.gameObject.SetActive(CheckIfTeleportActivated(controller));
+        }
+    }
+}
